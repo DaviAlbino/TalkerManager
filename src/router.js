7aa1e431ce59c27a6c48bc566a75fe15d4ac47de
Davi Albino <negroalbino_@hotmail.com>
2022-10-12 16:58:43 -0300

    Req 2 ok

--- src/router.js
+++ src/router.js
@@ -4,10 +4,23 @@ const router = express.Router();
 
 const read = require('./util/utilFs');
 
+const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
+ 
 router.get('/', async (_req, res) => {
     const talkersRead = await read();
-    console.log(talkersRead);
-    res.status(200).json(talkersRead);
+    res.status(HTTP_OK_STATUS).json(talkersRead);
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkersList = await read();
+    const talkerId = talkersList.filter((talker) => talker.id === Number(id));
+    if (talkerId.length > 0) {
+        res.status(HTTP_OK_STATUS).json(talkerId[0]);
+    } else {
+        res.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+    }
 });
 
 module.exports = router;
